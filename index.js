bce12aca91c13cf7bb53f38f3adc56e3e081f403
Sam-3453 <117625408+Sam-3453@users.noreply.github.com>
2022-11-20 23:56:11 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var img_content = document.getElementbyid('box');
+var image = ['1','2','3','4','5'];
+var i=image.length;
+
+function nextimge(){
+   if (i<image.length) {
+    i=i+1;
+   } else{
+    i=1;
+   }
+   img_content.innerHTML ="<img src="+image[i-1]+".jpg>";
+}
+function prcwimage(){
+    if (i<image.length+1 && i>1) {
+     i=i-1;
+    } else{
+     i=image.length;
+    }
+    img_content.innerHTML ="<img src="+image[i-1]+".jpg>";
+ }
+
+setInterval( nextimge , 1000);
